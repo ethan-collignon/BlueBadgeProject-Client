8b335aa99d7a412dd8a1e6894415421b5698846a
Ethan Collignon <89707534+ethan-collignon@users.noreply.github.com>
2021-10-29 16:35:33 -0400

    home added with nav

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.css'
 import './App.css';
-import {BrowserRouter as Router } from 'react-router-dom';
-import Navbar from './components/site/Navbar';
+import {BrowserRouter as Router} from 'react-router-dom';
+import Home from './components/site/Home1';
+import Navbar from './components/site/Navbar'
+// import Auth from './components/Auth/Auth';
 
 function App() {
+  const [sessionToken, setSessionToken] = useState(undefined);
+  console.log(sessionToken);
+  useEffect(() => {
+    if(localStorage.getItem('token')){
+      setSessionToken(localStorage.getItem('token'))
+    }
+  }, [])
+const updateLocalStorage = (newToken) => {
+  localStorage.setItem('token', newToken);
+  setSessionToken(newToken);
+};
+
+  const viewConductor = () => {
+    return sessionToken !== undefined ? <Home updateLocalStorage={updateLocalStorage} /> : <Home updateLocalStorage ={updateLocalStorage} />;
+  };
   return (
     <div className="App">
-       <Router>
-         <Navbar />
-       </Router>
+    <Router>
+      <Navbar />
+    </Router>
+      {viewConductor()}
     </div>
   );
 }
-
 export default App;
