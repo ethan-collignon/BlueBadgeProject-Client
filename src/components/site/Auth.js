117f91d59a7a0da19d9aaad822d0597ee194a256
Ethan Collignon <89707534+ethan-collignon@users.noreply.github.com>
2021-11-03 12:59:03 -0400

    1st page styling

--- src/components/site/Auth.js
+++ src/components/site/Auth.js
@@ -48,7 +48,15 @@ const Auth = (props) => {
       .catch((err) => console.log(err));
   };
   return (
-    <body style={{backgroundColor: "grey"}}>
+    <body style={{backgroundColor: "grey", height: "100vh"}}>
+      <div class="page">
+      <img
+        src={welcome}
+        alt="loading..."
+        height="400"
+        width="400"
+        className="welcome"
+      />
     <Form className="login-form">
       <h1>{title()}</h1>
       <FormGroup>
@@ -91,14 +99,7 @@ const Auth = (props) => {
       <div className="text-center">
         <a href="/forgot-password">Forgot Password?</a>
       </div>
-      <img
-        src={welcome}
-        alt="loading..."
-        height="400"
-        width="400"
-        className="welcome"
-      />
-      
+      </Form>
       <img
         src={intro}
         alt="loading..."
@@ -106,11 +107,12 @@ const Auth = (props) => {
         width="400"
         className="intro"
       />
-    </Form>
+      </div>
     <br />
     <br />
     <br />
-    
+    <br />
+    <br />
     <br />
     <br />
     </body>
@@ -131,81 +133,3 @@ export default Auth;
 
 
 
-
-
-// import React, { useState } from 'react';
-// import 'bootstrap/dist/css/bootstrap.css';
-
-// const Auth = (props) => {
-//     const [email, setEmail] = useState('');
-//     const [password, setPassword] = useState('');
-//     const [login, setLogin] = useState(true);
-
-// const title = () => {
-//     return !login ? 'Signup' : 'Login'
-// }
-
-// const loginToggle = (e) => {
-//     e.preventDefault();
-
-//     setLogin(!login)
-
-//     setEmail('');
-//     setPassword('');
-// }
-
-// const handleSubmit = event => {
-//     event.preventDefault();
-
-//     let reqBody = login ? 
-//      {
-//     user: {
-//         email: email,
-//         password: password
-//     }
-//     } : {
-//     user: {
-//         email: email,
-//         password: password,
-//     }
-//     }
-
-//     let url = login ? 
-//     'http://localhost:3001/user/login': 
-//     'http://localhost:3001/user/register';
-
-//     fetch(url, {
-//         method: 'POST', 
-//         body: JSON.stringify(reqBody),
-//         headers: new Headers({
-//             'Content-Type': 'application/json'
-//         }),
-//     })
-//     .then(response => response.json())
-//     .then(json => props.updateLocalStorage(json.sessionToken))
-//     .catch(err => console.log(err))
-// }
-
-//     return (
-//         <div>
-//             <form>
-//             <button onClick={loginToggle}>Login/Signup</button>
-//             <br/>
-//             <h1>{title()}</h1>
-//             <label htmlFor="email">Email:</label>
-//             <br/>
-//             <input type='text' id="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-//             <br/>
-//             <label htmlFor="password">Password:</label>
-//             <br/>
-//             <input type='password' id="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-//             <br/>
-//             <button type='submit' onClick={handleSubmit}>Submit</button>
-//             </form>
-//             {/* <img href="../../" alt="intro"/> */}
-//         </div>
-
-//     )
-// }
-
-// export default Auth;
